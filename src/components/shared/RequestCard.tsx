acbf620895660512c9d178deb2f68561616f8fcf
Kevin <hello@kevinivan.com>
2024-05-11 14:02:54 +0200

    build errors

--- src/components/shared/RequestCard.tsx
+++ src/components/shared/RequestCard.tsx
@@ -1,33 +1,7 @@
-import { Dot, Trash2 } from "lucide-react"
+import { Dot } from "lucide-react"
 import { Card } from "../ui/card"
-import { Button } from "../ui"
-import { useConfirm } from "./AlertDialogProvider"
-import { toast } from "sonner"
-import { useDeleteRequest } from "@/lib/react-query/queries"
-import { Badge } from "../ui/badge"
 
 const RequestCard = ({ request }) => {
-  const confirm = useConfirm()
-  const { mutateAsync: deleteRequest } = useDeleteRequest()
-
-  const handleDelete = async () => {
-    const declineConfirmed = await confirm({
-      title: `Are you sure you want to delete this request?`,
-      cancelButton: "Cancel",
-      actionButton: "Delete",
-    })
-
-    if (!declineConfirmed) return
-
-    try {
-      await deleteRequest({ requestId: request.$id })
-      toast.success("Request deleted successfully.")
-    } catch (error) {
-      toast.error("Could not delete request. Please try again.")
-      console.error(error)
-    }
-  }
-
   return (
     <Card className="flex items-center justify-between px-7">
       <div className="flex items-center gap-6">
