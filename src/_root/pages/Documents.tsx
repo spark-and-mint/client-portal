9e1c460a858898c57141741265448d0e380b4ff8
Kevin <hello@kevinivan.com>
2024-05-28 15:19:26 +0200

    update legal documents page

--- src/_root/pages/Documents.tsx
+++ src/_root/pages/Documents.tsx
@@ -2,7 +2,8 @@ import { Card } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 import { useStakeholderContext } from "@/context/AuthContext"
 import { useGetClientDocuments } from "@/lib/react-query/queries"
-import { ExternalLink, LinkIcon, Scale } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Dot, ExternalLink, LinkIcon, Scale } from "lucide-react"
 import FadeIn from "react-fade-in"
 import { Link } from "react-router-dom"
 
@@ -11,6 +12,39 @@ const Documents = () => {
 
   const { data: documents, isPending: isPendingDocuments } =
     useGetClientDocuments(stakeholder.clientId)
+
+  const statuses = {
+    yellow: ["Draft", "Pending Approval", "Under Negotiation", "Cancelled"],
+    purple: ["Amended", "Expired", "Archived"],
+    green: ["Signed", "Approved", "In Effect"],
+  }
+
+  const statusStyles = {
+    yellow: "text-amber-400",
+    purple: "text-purple-500",
+    green: "text-green-500",
+  }
+
+  const getStatusColor = (status: string) => {
+    for (const [color, statusArray] of Object.entries(statuses)) {
+      if (statusArray.includes(status)) {
+        return color
+      }
+    }
+    return null
+  }
+
+  const getStatus = (status: string) => {
+    const color = getStatusColor(status)
+    if (!color) return null
+
+    return (
+      <span className="flex flex-grow-0 items-center pr-3 pl-0.5 py-1 text-[0.9rem] border border-border rounded-lg text-xs font-medium tracking-wide">
+        <Dot className={cn("w-6 h-6 scale-150", statusStyles[color])} />
+        {status}
+      </span>
+    )
+  }
 
   return (
     <FadeIn className="pb-16 space-y-4">
@@ -31,22 +65,30 @@ const Documents = () => {
                 </p>
               </div>
 
-              <ul className="max-w-[26rem] divide-y divide-accent rounded-md border border-accent">
+              <ul className="divide-y divide-accent rounded-md border border-accent">
                 {documents?.documents.map((document) => (
-                  <Link to={document.link} target="_blank" className="block">
-                    <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6 hover:bg-accent/20">
-                      <div className="flex w-0 flex-1 items-center">
+                  <Link
+                    key={document.$id}
+                    to={document.link}
+                    target="_blank"
+                    className="block"
+                  >
+                    <li className="relative flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6 hover:bg-accent/20">
+                      <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-0">
                         <LinkIcon
-                          className="h-5 w-5 flex-shrink-0 text-gray-400"
+                          className="hidden sm:block h-5 w-5 flex-shrink-0 text-gray-400"
                           aria-hidden="true"
                         />
-                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                        <div className="sm:ml-4 flex items-center min-w-0 flex-1 gap-2">
                           <span className="truncate font-medium">
                             {document.title}
                           </span>
                         </div>
+                        <div className="flex items-center sm:ml-5">
+                          {getStatus(document.status)}
+                        </div>
                       </div>
-                      <div className="ml-4 flex font-medium text-primary items-center flex-shrink-0">
+                      <div className="ml-4 flex font-medium text-primary items-center">
                         Open
                         <ExternalLink className="h-4 w-4 mb-0.5 ml-2" />
                       </div>
