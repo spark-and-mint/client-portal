5e539969b5e8399b00e5a97facc95b4d6fb9e2f6
Kevin <hello@kevinivan.com>
2024-06-03 15:27:09 +0200

    fix lines

--- src/components/shared/Section.tsx
+++ src/components/shared/Section.tsx
@@ -43,14 +43,14 @@ const Section = ({
           opacity: showLines ? 1 : 0,
           transition: "opacity .5s ease-in-out",
         }}
-        className="hidden absolute top-0 left-5 w-0.25 h-full bg-accent/40 pointer-events-none md:block lg:left-7.5 xl:left-40"
+        className="hidden absolute top-0 left-5 w-0.25 h-full bg-accent/40 pointer-events-none md:block lg:left-[5.75rem] 2xl:left-[10rem]"
       />
       <div
         style={{
           opacity: showLines ? 1 : 0,
           transition: "opacity .5s ease-in-out",
         }}
-        className="hidden absolute top-0 right-5 w-0.25 h-full bg-accent/40 pointer-events-none md:block lg:right-7.5 xl:right-40"
+        className="hidden absolute top-0 right-5 w-0.25 h-full bg-accent/40 pointer-events-none md:block lg:right-[5.75rem] 2xl:right-[10rem]"
       />
       {crosses && (
         <div
@@ -62,7 +62,7 @@ const Section = ({
           <div
             className={`hidden absolute top-0 -left-7.5 right-8.5 h-0.25 bg-accent/40 ${
               crossesOffset && crossesOffset
-            } pointer-events-none lg:block xl:left0- right-0`}
+            } pointer-events-none lg:block right-0`}
           />
           <SectionSvg crossesOffset={crossesOffset} />
         </div>
